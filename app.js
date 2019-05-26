80aec3c872a576c351ce1935a50868b4e9bc84e1
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-05-26 10:58:14 +0530

    feat : express middleware

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const express = require('express');
 
 const app = express();
 
+// // How middleware works
+// app.use((req, res, next) => {
+//   // console.log(Date.now());
+//   req.name = 'Sumesh Majhi';
+//   next();
+// });
+
 // Index Route
 app.get('/', (req, res) => {
+  // console.log(req.name);
   res.send('INDEX');
 });
 
 // About Route
 app.get('/about', (req, res) => {
-  res.send('ABOUT ME');
+  res.send('ABOUT');
 });
 
 const port = 5000;
