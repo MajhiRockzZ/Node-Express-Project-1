af4e77f409787bd7308a58ee7980c7a918c414bb
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-06-01 19:59:17 +0530

    feat: install mongoose connect

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+// Connect to mongoose
+mongoose
+  .connect('mongodb://localhost/vidjot-dev', {
+    // useMongoClient: true
+    useNewUrlParser: true
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 // // How middleware works
 // app.use((req, res, next) => {
