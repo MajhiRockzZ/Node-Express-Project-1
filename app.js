b287a0e160f47fc62decfd17f0f3d5f0423b7bba
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-06-02 15:51:44 +0530

    feat: update idea data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -33,6 +34,9 @@ app.set('view engine', 'handlebars');
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Method override middleware
+app.use(methodOverride('_method'));
 
 // Index Route
 app.get('/', (req, res) => {
@@ -68,8 +72,7 @@ app.get('/ideas/add', (req, res) => {
 app.get('/ideas/edit/:id', (req, res) => {
   Idea.findOne({
     _id: req.params.id
-  })
-  .then(idea => {
+  }).then(idea => {
     res.render('ideas/edit', {
       idea: idea
     });
@@ -102,6 +105,21 @@ app.post('/ideas', (req, res) => {
       res.redirect('/ideas');
     });
   }
+});
+
+// Edit Form process
+app.put('/ideas/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    // new values
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+
+    idea.save().then(idea => {
+      res.redirect('/ideas');
+    });
+  });
 });
 
 const port = 5000;
